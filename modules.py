800f20af8acfee56b29fca6b58dd98a8172931af
Rebecca592 <rebeccaramotseoa@gmail.com>
2025-07-18 14:35:11 +0200

    Python FNB App Academy

---
+++ modules.py
@@ -0,0 +1,21 @@
+'''
+import math_operations
+
+print(math_operations.add(5, 2))
+
+print(math_operations.subtract(5,2))
+'''
+'''
+import math
+import random
+import datetime
+import os
+import sys
+
+#print(math.pi)
+#print(random.randint(0, 100))
+#print(datetime.datetime.now())
+#print(os.getcwd())
+print(sys.version)
+'''
+
---
+++ operators.py
@@ -0,0 +1,29 @@
+#Operators
+
+#Addition (+)
+#Subtraction (-)
+#Multiplication (*)
+#Division (/)
+#Modulus (%)
+#Exponents (**)
+'''
+x = 10
+y = 2 
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(5%2)
+
+print(x**y)
+'''
+
+x = 10
+x += 2
+
+print(x)
+
+x -= 2
+print(x)
